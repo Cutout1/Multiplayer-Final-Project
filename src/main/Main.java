9ec896a7c0a9d120c3cece2fe331bc9f0e135f78
shivamkmani <shivamkmani@10.56.242.49>
2018-05-07 08:59:08 -0700

    Yippee!

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String args[]) {
 	//run GUI to give player option to start a server or a client
-	System.out.println("Woohoo");
+	System.out.println("Woohoo! Yippee!");
 	StartUpGUI gui = new StartUpGUI();
 	}
 }
