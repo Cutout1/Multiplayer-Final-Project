eea62f78c8f0ada6687d131837a8c61f28a0169c
StinkyBunny42 <stinkybunny42@gmail.com>
2018-05-21 20:18:16 -0700

    hope this works

--- src/GUI/RockPaperScissorsGUI.java
+++ src/GUI/RockPaperScissorsGUI.java
@@ -25,16 +25,22 @@ public class RockPaperScissorsGUI implements ActionListener{
 	private JButton scissor;
 	private JLabel title;
 	private Client client;
+	private String myChoice;
+	private String otherPlayer;
+	private boolean otherPlayerHasMoved;
+	private String otherPlayerMove;
 	
 	BufferedReader in;
 	PrintWriter out;
 	
-	public RockPaperScissorsGUI(String otherPlayer, Client client1) {
+	public RockPaperScissorsGUI(String Player2, Client client1) {
 		client = client1;
-		initialize(otherPlayer);
+		otherPlayer = Player2;
+		otherPlayerHasMoved = false;
+		initialize();
 	}
 	
-	private void initialize(String otherPlayer) {
+	private void initialize() {
 		frame = new JFrame("Rock Paper Scissors");
 		frame.setBounds(0,0,750,300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -74,16 +80,62 @@ public class RockPaperScissorsGUI implements ActionListener{
 	public void actionPerformed(ActionEvent arg0) {
 		String command = arg0.getActionCommand();
 		if(command.equals("Rock")) {
-			
-			
+			client.send("ROCK");
+			myChoice = "ROCK";
+			if (otherPlayerHasMoved) {
+				check(otherPlayerMove);
+			}
 		}
 		if(command.equals("Paper")) {
-			
-			
+			client.send("PAPER");
+			myChoice = "PAPER";
+			if (otherPlayerHasMoved) {
+				check(otherPlayerMove);
+			}
 		}
-		if(command.equals("Paper")) {
-			
-			
+		if(command.equals("Scissors")) {
+			client.send("SCISSORS");
+			myChoice = "SCISSORS";
+			if (otherPlayerHasMoved) {
+				check(otherPlayerMove);
+			}
+		}
+	}
+	
+	public void check(String otherChoice) {
+		if (myChoice == null) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " has chosen his move, please pick");
+			otherPlayerHasMoved = true;
+			otherPlayerMove = otherChoice;
+		} else if(myChoice.equals(otherChoice)) {
+			JOptionPane.showMessageDialog(null, "You and " + otherPlayer + " have chosen the same move, pick again");
+			myChoice = null;
+			otherPlayerHasMoved = false;
+			otherPlayerMove = null;
+		} else if(myChoice.equals("ROCK") && otherChoice.equals("PAPER")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose paper, and you chose rock, so you lose :(");
+			client.showLobby();
+			frame.dispose();
+		} else if(myChoice.equals("ROCK") && otherChoice.equals("SCISSORS")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose scissors, and you chose rock, so you win! :)");
+			client.showLobby();
+			frame.dispose();
+		} else if(myChoice.equals("PAPER") && otherChoice.equals("SCISSORS")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose scissors, and you chose paper, so you lose :(");
+			client.showLobby();
+			frame.dispose();
+		} else if(myChoice.equals("PAPER") && otherChoice.equals("ROCK")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose paper, and you chose paper, so you win! :)");
+			client.showLobby();
+			frame.dispose();
+		} else if(myChoice.equals("SCISSORS") && otherChoice.equals("PAPER")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose paper, and you chose scissors, so you win! :)");
+			client.showLobby();
+			frame.dispose();
+		} else if(myChoice.equals("SCISSORS") && otherChoice.equals("ROCK")) {
+			JOptionPane.showMessageDialog(null, otherPlayer + " chose rock, and you chose scissors, so you lose :(");
+			client.showLobby();
+			frame.dispose();
 		}
 	}
 }
---
+++ src/battleShips/BattleShips.java
@@ -0,0 +1,86 @@
+package battleShips;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
+import ticTacToe.TTTDraw;
+
+public class BattleShips extends Thread implements MouseListener, KeyListener {
+	
+	private int[][] myPoints = new int[10][10];
+	
+	private BSDraw BSD;
+	private JFrame frame;
+	
+	public BattleShips() {
+		
+	}
+	
+	public void run() {
+		BSD = new BSDraw();
+        frame = new JFrame("Battle Ships");
+        frame.add(BSD);
+        frame.pack();
+        frame.setVisible(true);
+        
+        frame.addMouseListener(this);
+        frame.addKeyListener(this);
+        
+        BSD.updatePoints(myPoints);
+        BSD.repaint();
+        
+	}
+	
+	private void click(int x, int y) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent key) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
